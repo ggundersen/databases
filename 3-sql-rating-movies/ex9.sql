efcf815e40238807459efe453fbfcd340cb496a6
gwg <ggundersen@gmail.com>
2016-03-15 07:45:58 -0400

    Formatting

--- 3-sql-rating-movies/ex9.sql
+++ 3-sql-rating-movies/ex9.sql
@@ -2,7 +2,7 @@
  * Exercise 9:
  * Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980.
  * (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after.
- * Don't just calculate the overall average rating before and after 1980.) 
+ * Don't just calculate the overall average rating before and after 1980.)
  */
 
 SELECT preAvgStars - postAvgStars
