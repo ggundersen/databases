1b61a94eef809df59273b4c014ec26c13ef1e549
gwg <ggundersen@gmail.com>
2016-07-06 08:15:48 -0400

    Exercise 5.

---
+++ 10-constraints_and_triggers/social_network_triggers/ex5.sql
@@ -0,0 +1,28 @@
+/*
+ * Write a trigger that automatically deletes students when they graduate, i.e.,
+ * when their grade is updated to exceed 12 (same as Question 4). In addition, 
+ * write a trigger so when a student is moved ahead one grade, then so are all 
+ * of his or her friends. 
+ */
+
+CREATE TRIGGER graduate
+AFTER UPDATE ON Highschooler
+FOR EACH ROW
+WHEN New.grade > 12
+BEGIN
+    DELETE FROM Highschooler
+    WHERE ID = New.ID;
+END;
+
+| -- This pipe is required by the Lagunitas grading system.
+
+CREATE TRIGGER newyear
+AFTER UPDATE ON Highschooler
+WHEN New.grade = Old.grade + 1
+BEGIN
+    UPDATE Highschooler
+    SET grade = grade + 1
+    WHERE ID IN (
+        SELECT ID2 FROM Friend WHERE ID1 = Old.ID
+    );
+END;
