08823ae91875a43e62b160b0d770e72a760ec7e2
Greg <ggundersen@gmail.com>
2016-07-14 18:15:15 -0400

    Formatting.

--- 11-views_and_authorization/views_exercises/ex1.sql
+++ 11-views_and_authorization/views_exercises/ex1.sql
@@ -2,7 +2,7 @@
  * Write an instead-of trigger that enables updates to the title attribute of 
  * view LateRating.
  *
- * Policy:** Updates to attribute title in LateRating should update Movie.title 
+ * Policy: Updates to attribute title in LateRating should update Movie.title
  * for the corresponding movie. (You may assume attribute mID is a key for 
  * table Movie.) Make sure the mID attribute of view LateRating has not also 
  * been updated -- if it has been updated, don't make any changes. Don't worry 
